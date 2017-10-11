5c24f535bb6d8ecb7e1aa789ae1e786fa885d740
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-10-11 13:54:38 +0700

    add some configs

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const express = require("express")
+const webpack = require("webpack")
+const webpackMiddleware = require("webpack-dev-middleware")
+const path = require("path")
+
+const webpackConfig = require("./webpack.config")
+
+const compiler = webpack(webpackConfig)
 const app = express()
 
-app.get("/", function(req, res) {
-  res.send("Hello World")
+app.use(webpackMiddleware(compiler))
+
+app.use(express.static(path.resolve("/", "public")))
+
+app.use("*", function(req, res, next) {
+  const filename = path.join(compiler.outputPath, "index.html")
+  compiler.outputFileSystem.readFile(filename, function(err, result) {
+    if (err) {
+      return next(err)
+    }
+
+    res.set("Content-Type", "text/html")
+    res.send(result)
+    res.end()
+  })
 })
 
 app.listen(3000, function() {
